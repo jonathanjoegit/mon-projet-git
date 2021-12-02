df1ac76c447730aab2d9da05d963d10c17b1d45d
jonathanjoegit <jonathan.jupin@univ-lemans.fr>
2021-12-02 15:41:03 +0100

    Ajout de mon projet de création de table en bois massif

---
+++ projet-table-bois.py
@@ -0,0 +1,5 @@
+# Projet de table en bois massif pour ma salle à manger
+# il me faut :
+
+outils = ['scie sauteuse', 'tournevis', 'ponceuse', "perceuse" ]
+materiaux = ['1 pleateau en chêne', '4 chevrons', '50 vis 6x140mm', "un pot de cire" ]
